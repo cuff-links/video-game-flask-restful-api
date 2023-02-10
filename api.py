e8d1eda6554a0cc8cbb54375533a6a1d50654583
John Dorlus <jsdorlus@gmail.com>
2023-02-09 23:29:24 -0500

    Fixed typo.

--- api.py
+++ api.py
@@ -110,4 +110,4 @@ api.add_resource(GameList, "/games")
 api.add_resource(Game, "/game/<int:id>")
 
 if __name__ == '__main__':
-    app.run('0.0.0.0.', debug=True)
+    app.run(host='0.0.0.0', debug=True)
